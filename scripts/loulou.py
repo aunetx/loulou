6e3c5b681a089582fee2c1f6fa9fc9b63360e805
Aurélien Hamy <aunetx@users.noreply.github.com>
2019-02-11 23:04:14 +0100

    Added epochs progression printing and reduce-output option

--- scripts/loulou.py
+++ scripts/loulou.py
@@ -2,6 +2,8 @@ import numpy as np
 import json
 import os
 import mnist
+import sys
+from tqdm import tqdm
 
 def feed_forward(X, weights):
     a = [X]
@@ -19,32 +21,45 @@ def grads(X, Y, weights):
         grads[i-1] = a[i-1].T.dot(delta) # calculating errors of weights and storing onto |grads|
     return grads / len(X)
 
-def train(weights, trX, trY, teX, teY, filename, epochs, batch, learning_rate, save_timeout):
+def train(weights, trX, trY, teX, teY, filename, epochs, batch, learning_rate, save_timeout, reduce_output):
     path = os.path.dirname(__file__)
     accuracy = {}
     prediction = np.argmax(feed_forward(teX, weights)[-1], axis=1)
     accuracy[0] = np.mean(prediction == np.argmax(teY, axis=1))
-    print(0, accuracy[0])
+    if reduce_output < 2:
+        print('Accuracy of epoch',0,':', accuracy[0])
+    if epochs < 0:
+        epochs = 99999999999
     for i in range(epochs):
-        for j in range(0, len(trX), batch):
+        if reduce_output < 1:
+            pbar = tqdm(range(0, len(trX), batch))
+        else:
+            pbar = range(0, len(trX), batch)
+        for j in pbar:
+            if reduce_output < 1:
+                pbar.set_description("Processing epoch %s" %(i+1))
+
             X, Y = trX[j:j+batch], trY[j:j+batch]
             weights -= learning_rate * grads(X, Y, weights)
         prediction = np.argmax(feed_forward(teX, weights)[-1], axis=1)
         accuracy[i+1] = np.mean(prediction == np.argmax(teY, axis=1))
-        print(i+1, accuracy[i+1])
+        if reduce_output < 2:
+            print('Accuracy of epoch',i+1,':', accuracy[i+1])
         if filename:
-            if i % save_timeout == 0:
-                temp_filename = '../trains/temp/' + filename + '_epoch_' + str(i) + '.npy'
-                temp_filename = os.path.join(path, temp_filename)
-                save(weights, temp_filename)
+            if save_timeout > 0:
+                if i % save_timeout == 0:
+                    temp_filename = '../trains/temp/' + filename + '_epoch_' + str(i) + '.npy'
+                    temp_filename = os.path.join(path, temp_filename)
+                    save(weights, temp_filename, reduce_output)
     if filename:
         filename = os.path.join(path, '../trains/' + filename + '.npy')
-        save(weights, filename)
+        save(weights, filename, reduce_output)
     return accuracy
 
-def save(weights, filename):
+def save(weights, filename, reduce_output):
     np.save(filename,weights)
-    print('Data saved successfully into ',filename)
+    if reduce_output < 2:
+        print('Data saved successfully into ',filename)
 
 def convertJson(pred):
     out = {}
@@ -58,9 +73,10 @@ def runTrain(params, architecture, file=None):
     batch = params['batch']
     learning_rate = params['learning_rate']
     save_timeout = params['save_timeout']
+    reduce_output = params['reduce_output']
     trX, trY, teX, teY = mnist.load_data()
     weights = [np.random.randn(*w) * 0.1 for w in architecture]
-    return train(weights, trX, trY, teX, teY, file, epochs, batch, learning_rate, save_timeout)
+    return train(weights, trX, trY, teX, teY, file, epochs, batch, learning_rate, save_timeout, reduce_output)
 
 def listToArch(list):
     arch = []
--- scripts/train.py
+++ scripts/train.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     params['batch'] = 20
     params['learning_rate'] = 0.03
     params['save_timeout'] = 5
-    architecture = [(784, 200), (200, 100), (100, 10)]
+    params['reduce_output'] = 0
+    architecture = [(784, 10)]
     filename = None
 
     #   Parser
@@ -18,15 +19,17 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--filename', dest='filename', type=str,
         help='name of the file to write, extension added automatically (default : none)') #    Need to set option for other paths
     parser.add_argument('-e', '--epochs', dest='epochs', type=int,
-        help='number of epochs (default : 15, -1 for infinity)') #  Need to set for -1
+        help='number of epochs (default : 15, -1 for infinity)')
     parser.add_argument('-b', '--batch-size', dest='batch_size', type=int,
         help='size of batch (default : 20)')
     parser.add_argument('-l', '--learning-rate', dest='learning_rate', type=float,
         help='learning rate of the network (default : 0.03)')
     parser.add_argument('-s', '--save-timeout', dest='save_timeout', type=int,
-        help='number N of epochs to save automatically the file as filename_epoch_N (default : 5, 0 to disable)') #  Need to set for 0
+        help='number N of epochs to save automatically the file as filename_epoch_N (default : 0, disabled)') #  Need to set for 0
     parser.add_argument('-a', '--architecture', nargs='+', type=int,
-        help='architecture of the hidden layers (default : 200 100)') #   Need to set option for no hidden layer
+        help='architecture of the hidden layers (default : none)') #   Need to set option for no hidden layer
+    parser.add_argument('-r', '--reduce-output', dest='reduce_output', action="count",
+        help='reduce verbosity of output (you can type several)')
     args = parser.parse_args()
 
     if args.epochs is not None:
@@ -41,6 +44,8 @@ if __name__ == '__main__':
         filename = args.filename
     if args.save_timeout is not None:
         params['save_timeout'] = args.save_timeout
+    if args.reduce_output is not None:
+        params['reduce_output'] = args.reduce_output
 
     params = json.dumps(params)
     accuracy = runTrain(params, architecture, file=filename)
